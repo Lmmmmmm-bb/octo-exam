22c22a8c12aa637cee1c325a8b0b875630258578
_lmmmmmm <931420654@qq.com>
2022-03-14 20:40:52 +0800

    [修复]: 修复 not-found 页面返回路径错误的问题

--- src/layouts/not-found/index.vue
+++ src/layouts/not-found/index.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
+import { RouterNameEnum } from '@/router/type';
 import { ElEmpty, ElLink } from 'element-plus';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const handleReturnLogin = () => router.push({ name: RouterNameEnum.Login });
 </script>
 
 <template>
   <ElEmpty style="height: 100%" description="Not Found">
-    <ElLink type="primary" href="/login">返回登陆</ElLink>
+    <ElLink type="primary" @click="handleReturnLogin">返回登陆</ElLink>
   </ElEmpty>
 </template>
