a3d6147ddddb163eb97a4ab1b2632d5078551a27
_lmmmmmm <931420654@qq.com>
2022-03-27 16:50:51 +0800

    [修复](element-ui): 修复复制弹窗 element 样式未导入的问题

--- src/common/utils/copy.ts
+++ src/common/utils/copy.ts
@@ -1,23 +1,20 @@
-import { ElNotification } from 'element-plus';
+import { ElMessage } from 'element-plus';
 
 export const copyable = async (text: string) => {
   const clipboard = navigator.clipboard;
   if (clipboard) {
     try {
       await clipboard.writeText(text);
-      ElNotification.success({
-        title: '提示',
+      ElMessage.success({
         message: '已成功复制到剪贴板'
       });
     } catch (err) {
-      ElNotification.error({
-        title: '提示',
+      ElMessage.error({
         message: '复制失败，请稍后重试'
       });
     }
   } else {
-    ElNotification.warning({
-      title: '提示',
+    ElMessage.warning({
       message: '当前浏览器版本过低，请升级浏览器后重试'
     });
   }
